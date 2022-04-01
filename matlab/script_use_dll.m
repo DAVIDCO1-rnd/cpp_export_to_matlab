14c3069e644f303a2ad29831be209b7bd450e9b5
David Cohn <DAVIDCO1@rnd-hub.com>
2022-04-01 23:18:33 +0300

    works for pure c file

--- matlab/script_use_dll.m
+++ matlab/script_use_dll.m
@@ -2,16 +2,19 @@ clear;
 close all;
 clc;
 
-is_dll_loaded = libisloaded('Cpp_to_matlab_DLL');
+is_dll_loaded = libisloaded('Cpp_to_matlab_SHARED');
 
 if is_dll_loaded==true
-    unloadlibrary('Cpp_to_matlab_DLL');
+    unloadlibrary('Cpp_to_matlab_SHARED');
 end
 
-[notfound, warnings] = loadlibrary('Cpp_to_matlab_DLL.dll', 'calc_sum.h');
+[notfound, warnings] = loadlibrary('Cpp_to_matlab_SHARED.dll', 'calc_sum.h');
 
-num1 = int32(5);
+num1 = int32(9);
 num2 = int32(7);
-res = calllib('Cpp_to_matlab_DLL','my_func',num1, num2);
+res = calllib('Cpp_to_matlab_SHARED','my_func',num1, num2);
 fprintf('%d + %d = %d\n',num1, num2, res);
+
+unloadlibrary('Cpp_to_matlab_SHARED');
+
 
