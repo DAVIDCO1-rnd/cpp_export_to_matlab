37699b071853fefa563934cb6e565f4accbb3bbe
David <davidco1-rnd@hotmail.com>
2022-04-10 15:11:21 +0300

    Updated calc_sum.h to support also Linux

--- program1/DLL_folder/Includes/calc_sum.h
+++ program1/DLL_folder/Includes/calc_sum.h
@@ -1,5 +1,12 @@
-
-#define EXPORT __declspec (dllexport)
+#ifdef _WIN32
+#  ifdef MODULE_API_EXPORTS
+#    define EXPORT __declspec(dllexport)
+#  else
+#    define EXPORT __declspec(dllimport)
+#  endif
+#else
+#  define EXPORT
+#endif
 
 EXPORT int my_func(int num1, int num2);
 
